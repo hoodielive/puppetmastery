74c5da53c89a2741a3a2e479fc47b7f958d6f7cb
hoodielive <hoodielive@gmail.com>
2018-08-11 19:37:21 -0400

    cant get anything done because the damn phone keeps ringing

--- pam/manifests/init.pp
+++ pam/manifests/init.pp
@@ -1,6 +1,12 @@
 # This PAM login file is managed by puppet, so don't be a dofus and edit it here because your changes will be
 # 'overwritten'! u dig? 
 
+class pam {
+    package { 'pam':
+
+
+
+
 file { 'login':
     path    => '/etc/pam.d/login',
     ensure  => file, 

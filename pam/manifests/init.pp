0b58da69211bf545ec971e5052c1b0137a1facc0
hoodielive <hoodielive@gmail.com>
2018-08-14 12:21:33 -0400

    expanding notes - modules and the anatomy thereof

--- pam/manifests/init.pp
+++ pam/manifests/init.pp
@@ -1,16 +1,11 @@
 # This PAM login file is managed by puppet, so don't be a dofus and edit it here because your changes will be
 # 'overwritten'! u dig? 
 
-class pam {
-    package { 'pam':
-
-
-
 
 file { 'login':
     path    => '/etc/pam.d/login',
     ensure  => file, 
-    content => template('pam/login'),
+    content => template('pam/login.centos.erb'),
     owner   => 'root',
     mode    => '0644',
 }

1d477b218a2d0d3b1bc47d73fc04b952b2fc7bc6
hoodielive <hoodielive@gmail.com>
2019-06-23 16:59:53 -0400

    worked on the puppet-amanda module a little bit

--- amanda-dev/init.pp
+++ amanda-dev/init.pp
@@ -1,22 +1,34 @@
-class amanda (
-   Optional[String] $config_dir, 
-   Optional[String] $backup_dir, 
-   Optional[Integer[0, 65535] $uid, 
-   Optional[Integer[0, 65535] $user, 
-   Optional[String] $comment, 
-   Optional[String] $shell,
-   Optional[String] $group,
-   Array[String] $groups,
-   Optional[String] $server_package,
-   Optional[String] $client_package,
-   Boolean $server_provides_client,
-   Optional[String] $amandad_path,
-   Optional[String] $amandaidx_path,
-   Optional[String] $amandataped_path,
-   Array[String] $amanda_directories,
-) {
-    if $facts['RedHat'] {
+class amanda::server (
+  Struct[{
+    Optional[String] $config_dir, 
+    Optional[String] $backup_dir, 
+    Optional[Integer[0, 65535] $uid, 
+    Optional[Integer[0, 65535] $user, 
+    Optional[String] $comment, 
+    Optional[String] $shell,
+    Optional[String] $group,
+    Array[String] $groups,
+    Optional[String] $server_package,
+    Optional[String] $client_package,
+    Boolean $server_provides_client,
+    Optional[String] $amandad_path,
+    Optional[String] $amandaidx_path,
+    Optional[String] $amandataped_path,
+    Array[String] $amanda_directories,
+  }] $amanda_config = {}
+)  inherits amanda { 
+     if $facts['RedHat'] {
         
-    } else {
+   } else {
+    
+   # Manage the /etc/amanda/SeiConfig file. 
+   file { '/etc/amanda/SeiConfig':
+     ensure    => file, 
+     require   => Package['amanda-server'], 
+     owner     => amandabackup,
+     group     => amandabackup,
+     group     => '0644',
+     content   => template("amanda/server/amanda.conf.erb"),
+}
 
-  }
+

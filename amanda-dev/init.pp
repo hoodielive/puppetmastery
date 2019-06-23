676661ea3d54d296600c024ca03d6b31f3d83d0a
hoodielive <hoodielive@gmail.com>
2019-06-23 17:03:45 -0400

    removed \s$ and put comments in file

--- amanda-dev/init.pp
+++ amanda-dev/init.pp
@@ -1,10 +1,18 @@
+
+# == Class: amanda::server 
+#
+# This class installs/configures/manages the Amanda server.
+#
+# === Parameters
+#
+
 class amanda::server (
   Struct[{
-    Optional[String] $config_dir, 
-    Optional[String] $backup_dir, 
-    Optional[Integer[0, 65535] $uid, 
-    Optional[Integer[0, 65535] $user, 
-    Optional[String] $comment, 
+    Optional[String] $config_dir,
+    Optional[String] $backup_dir,
+    Optional[Integer[0, 65535] $uid,
+    Optional[Integer[0, 65535] $user,
+    Optional[String] $comment,
     Optional[String] $shell,
     Optional[String] $group,
     Array[String] $groups,
@@ -16,19 +24,24 @@ class amanda::server (
     Optional[String] $amandataped_path,
     Array[String] $amanda_directories,
   }] $amanda_config = {}
-)  inherits amanda { 
+)  inherits amanda {
      if $facts['RedHat'] {
-        
+
    } else {
-    
-   # Manage the /etc/amanda/SeiConfig file. 
+
+   # Manage the /etc/amanda/SeiConfig file.
    file { '/etc/amanda/SeiConfig':
-     ensure    => file, 
-     require   => Package['amanda-server'], 
+     ensure    => file,
+     require   => Package['amanda-server'],
      owner     => amandabackup,
      group     => amandabackup,
      group     => '0644',
      content   => template("amanda/server/amanda.conf.erb"),
 }
 
-
+#
+# Local Variables:
+# mode:Puppet
+# fill-column:79
+# End:
+#
